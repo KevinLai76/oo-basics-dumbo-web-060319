803cec6fa64e67d8b584efe6a0dbb955b438ca72
Kevin <kevinlai76@gmail.com>
2019-06-05 03:54:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,38 @@
-# Make your shoe class here!
+require "pry"
+
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    binding.pry
+    if cobble == "old"
+      cobble =="new"
+    end 
+  end
+  
+end
+
+# class Person
+ 
+#   def initialize(name)
+#     @name = name
+#   end
+ 
+#   def name
+#     @name
+#   end
+ 
+#   def name=(new_name)
+#     @name = new_name
+#   end
+ 
+# end
